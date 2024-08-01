1c0022eb51a5230cef4761087e704c7769cfacbd
SlickYeet <lasse@famlam.ca>
2024-07-31 20:21:31 -0700

    feat: Add grainy background to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,16 @@ import { buttonVariants } from "@/components/ui/button"
 
 const Home = () => {
   return (
-    <div className="bg-slate-50">
+    <div className="grainy-light bg-slate-50">
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-3 lg:gap-x-0 lg:pb-52 lg:pt-24 xl:gap-x-8 xl:pt-32">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto flex flex-col items-center text-center lg:items-start lg:text-left">
               <div className="absolute -top-20 left-0 hidden w-28 lg:block">
+                <div
+                  aria-hidden="true"
+                  className="absolute inset-x-0 bottom-0 h-28 bg-gradient-to-t from-slate-50 via-slate-50/50"
+                />
                 <img
                   src="/snake-1.png"
                   alt="CaseCobra logo"
